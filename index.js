de9d3e525408d95739594bf2640e86f4b9c670c0
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-10-09 15:57:21 +0200

    1.2 : Go to first course

--- index.js
+++ index.js
@@ -11,18 +11,31 @@ function delay(time) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless : false});
+  const browser = await puppeteer.launch({ headless : false, defaultViewport: null});
   const page = await browser.newPage();
 
   await page.goto(memrise_url, {waitUntil: "networkidle2"});
 
-  await page.type("input[id='username'", process.env.USER_NAME, {delay: 75})
-  await page.type("input[id='password'", process.env.USER_PASSWORD, {delay: 75})
+  await page.type("input[id='username'", process.env.USER_NAME, {delay: 1})
+  await page.type("input[id='password'", process.env.USER_PASSWORD, {delay: 1})
   await page.click("button[data-testid='signinFormSubmit']");
 
   await delay(2000);
   await page.goto(course_url, {waitUntil: "networkidle2"});
   await page.click(".cc-btn.cc-allow");
+
+  let pathname = "/" + course_url.split("/").splice(3,3).join("/") + "/";
+  let current_course = pathname + "1/";
+  let selector = "a[href='"+ current_course + "']" 
+  await page.click(selector);
+
+  let data = await page.evaluate(() => {
+    return document.querySelector(".thing.text-text > .col.text > .text").innerHTML;
+  });
+
+  console.log(data)
+  
+  
   
 
   //await browser.close();
