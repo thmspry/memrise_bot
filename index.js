d1d1af3575a25bf42e3e4ea69044a31657875290
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-10-09 13:58:49 +0200

    1.1 : Log with .env logins

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const url_test = "https://app.memrise.com/course/2233959/3a-s1-polytech-nantes/";
+const memrise_url = "https://app.memrise.com/signin"
+const course_url = "https://app.memrise.com/course/2233959/3a-s1-polytech-nantes/";
+
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
 
 (async () => {
   const browser = await puppeteer.launch({ headless : false});
   const page = await browser.newPage();
-  await page.goto(url_test, {waitUntil: "networkidle2"});
 
-  await browser.close();
+  await page.goto(memrise_url, {waitUntil: "networkidle2"});
+
+  await page.type("input[id='username'", process.env.USER_NAME, {delay: 75})
+  await page.type("input[id='password'", process.env.USER_PASSWORD, {delay: 75})
+  await page.click("button[data-testid='signinFormSubmit']");
+
+  await delay(2000);
+  await page.goto(course_url, {waitUntil: "networkidle2"});
+  await page.click(".cc-btn.cc-allow");
+  
+
+  //await browser.close();
 })();
