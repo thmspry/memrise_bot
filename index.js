2b47e88fc07470550e8a388e30b39ea860c68b17
Thomas Peray <thomas.peray5@gmail.com>
2021-10-10 14:24:26 +0200

    1.7 : Letter case works

--- index.js
+++ index.js
@@ -5,30 +5,44 @@ const memrise_url = "https://app.memrise.com/signin"
 const course_url = "https://app.memrise.com/course/2233959/3a-s1-polytech-nantes/";
 
 function delay(time) {
-  return new Promise(function(resolve) { 
-      setTimeout(resolve, time)
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
   });
+}
+
+function getTranslation(dico, word) {
+  let res = "";
+  dico.forEach(w => {
+    if (w.original == word) {
+      res = w.translated;
+    }
+
+    if (w.translated == word) {
+      res = w.original;
+    }
+  })
+  return res;
 }
 
 (async () => {
 
   // Initialisation
-  const browser = await puppeteer.launch({ headless : false, defaultViewport: null});
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
   const page = await browser.newPage();
 
-  await page.goto(memrise_url, {waitUntil: "networkidle2"});
+  await page.goto(memrise_url, { waitUntil: "networkidle2" });
 
-  await page.type("input[id='username'", process.env.USER_NAME, {delay: 1})
-  await page.type("input[id='password'", process.env.USER_PASSWORD, {delay: 1})
+  await page.type("input[id='username']", process.env.USER_NAME, { delay: 1 })
+  await page.type("input[id='password']", process.env.USER_PASSWORD, { delay: 1 })
   await page.click("button[data-testid='signinFormSubmit']");
 
   await delay(2000);
-  await page.goto(course_url, {waitUntil: "networkidle2"});
+  await page.goto(course_url, { waitUntil: "networkidle2" });
   await page.click(".cc-btn.cc-allow");
 
-  let pathname = "/" + course_url.split("/").splice(3,3).join("/") + "/";
+  let pathname = "/" + course_url.split("/").splice(3, 3).join("/") + "/";
   let current_course = pathname + "1/";
-  let selector = "a[href='"+ current_course + "']" 
+  let selector = "a[href='" + current_course + "']"
   await page.click(selector);
 
   await delay(2000);
@@ -41,18 +55,28 @@ function delay(time) {
     els.forEach(e => {
       motsBrut.push(e.innerText)
     })
-    for(let i=0; i<motsBrut.length; i=i+2) {
+    for (let i = 0; i < motsBrut.length; i = i + 2) {
       mots.push({
         original: motsBrut[i],
-        translated: motsBrut[i+1]
+        translated: motsBrut[i + 1]
       })
     }
     return mots
   });
 
   // Main loop
-  await page.click("a.btn.btn-light-green");
+  try {
+    await page.click("a.btn.btn-light-green");
+  } catch (error) {
+    console.log("No light green button :", error)
+  }
 
+  try {
+    await page.click(".actions.actions-right > a");
+  } catch (error) {
+    console.log("No simple button :", error)
+  }
+  
   await delay(2000);
 
   let currentWord = await page.evaluate(() => {
@@ -60,33 +84,65 @@ function delay(time) {
     return e.innerText
   });
 
+
+
   let letterCase = await page.evaluate(() => {
     let e = document.querySelector(".sc-ojuw87-1.kclydn");
     if (e) {
-      return e.innerText 
+      return e.innerText
     }
     return null
   });
 
   let wordCase = await page.evaluate(() => {
-    let e = document.querySelector(".sc-1opiu1v-0.dzOzSf");
+    let e = document.querySelector(".sc-7v3i35-1.cmZgNh");
     if (e) {
-      return e.innerText 
+      return e.innerText
     }
     return null
   });
 
-  const currentWordSplited = currentWord.split("");
-  /*if (letterCase) {
-    for(let i=0; i<currentWordSplited.length; i++) {
-      await page.click("button[data-testid=typing-response-" + currentWordSplited[i] + "-2");
+  let sentenceCase = await page.evaluate(() => {
+    let e = document.querySelector(".sc-1opiu1v-0.dzOzSf");
+    if (e) {
+      return e.innerText
     }
-  }*/
+    return null
+  });
 
-  
-  
-  
-  
+  const translation = getTranslation(dico, currentWord);
+  console.log("Translation :", translation)
+
+  if (letterCase) {
+    await page.type(".sc-ojuw87-2.TpKoe", translation, { delay: 1 })
+    await page.click(".sc-bdfBQB.kMSUVe")
+  }
+
+  if (wordCase) {
+    console.log("wordCase :", wordCase)
+    let translationSplited = translation.split(" ");
+    let currentWordSplited = currentWord.split(" ");
+
+
+    
+  }
+
+  if (sentenceCase) {
+
+    let sentences = await page.evaluate(() => {
+      let els = document.querySelectorAll(".sc-bdfBQB.dvaGCW");
+      els.forEach(e => {
+        if (e.innerText.includes(translation)) {
+
+        }
+      })
+    });
+  }
+
+
+
+
+
 
   //await browser.close();
 })();
